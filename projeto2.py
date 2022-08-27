f8ac56c846ca3399a43f539c17d0044eace5f375
Mateus <munak98@hotmail.com>
2022-08-27 13:33:32 -0300

    matching 76, not school saturated

--- projeto2.py
+++ projeto2.py
@@ -8,31 +8,16 @@ class Professor():
         self.contratante = ""
         self.rejeicoes = []
     
-    def info(self):
-        print("ID", self.id)
-        print("Habilitacoes", self.habilitacoes)
-        print("Preferencias", self.preferencias)
-        print("Contratante", self.contratante)
-        print("Rejeicoes", self.rejeicoes)
-
-    
-
 class Escola():
     def __init__(self, id, habilitacoes):
         self.id = id
         self.habilitacoes_pretendidas = [int(x) for x in habilitacoes]
         self.habilitacoes_obtidas = []
-        self.vagas = len(habilitacoes)
         self.contratados = []
-
-    def info(self):
-        print("ID", self.id)
-        print("Habilitacoes desejadas", self.habilitacoes_pretendidas)
-        print("Contratados", self.contratados)
-        print("Habilitacoes obtidas", self.habilitacoes_obtidas)
+        self.candidatos_possiveis = []
 
 
-# lê dados do arquivo e guarda os professores e escolas em duas listas        
+# lê dados do arquivo e guarda os professores e escolas em dois dicionarios        
 with open("projetoTAG.txt", "r") as file:
     professores = {}
     escolas = {}
@@ -46,70 +31,71 @@ with open("projetoTAG.txt", "r") as file:
         if line[0].startswith("E"):
             escolas[line[0]] = Escola(line[0], line[1:])
 
-
-def melhor_escola_possivel(prof):
-    for esc_id in prof.preferencias:
-        if prof.habilitacoes >= min(escolas[esc_id].habilitacoes_pretendidas):
-            return esc_id
-
-
-def condition(professores):
+def desempregados_com_opcao(professores):
     aplicantes = []
     for prof in professores.values():
-        if prof.contratado == 0 and (prof.preferencias != []):
+        if prof.contratado == 0 and (len(prof.preferencias) != len(prof.rejeicoes)):
             aplicantes.append(prof.id)
     return aplicantes
 
+def contrata(esc_id, prof_id):
+    escolas[esc_id].contratados.append(prof_id)
+    escolas[esc_id].habilitacoes_obtidas.append(professores[prof_id].habilitacoes)
 
-def dispensa(idx, esc_id):
+    professores[prof_id].contratado = 1
+    professores[prof_id].contratante = esc_id
+
+def dispensa(esc_id, idx):
     prof_id = escolas[esc_id].contratados[idx]
     professores[prof_id].contratado = 0
     professores[prof_id].contratante = ""
     professores[prof_id].rejeicoes.append(esc_id)
-    professores[prof_id].preferencias.remove(esc_id)
 
     escolas[esc_id].contratados.pop(idx)
     escolas[esc_id].habilitacoes_obtidas.pop(idx)
+    escolas[esc_id].candidatos_possiveis.remove(prof_id)
 
-def restringir_preferencias():
-    for prof_id in professores:
-        to_remove = []
-        for esc_id in professores[prof_id].preferencias:
-            if professores[prof_id].habilitacoes < min(escolas[esc_id].habilitacoes_pretendidas):
-                to_remove.append(esc_id)
-        for x in to_remove:
-            professores[prof_id].preferencias.remove(x)
-
+def melhor_escola_possivel(prof_id):
+    for esc_id in professores[prof_id].preferencias:
+        if prof_id in escolas[esc_id].candidatos_possiveis:
+            return esc_id
+    return -1
 
 def emparelhamentoEstavel():
-    restringir_preferencias()
-    disponiveis = condition(professores)
+    for prof_id in professores:
+        for esc_id in professores[prof_id].preferencias:
+            if professores[prof_id].habilitacoes >= min(escolas[esc_id].habilitacoes_pretendidas):
+                escolas[esc_id].candidatos_possiveis.append(prof_id)
+            else:
+                professores[prof_id].rejeicoes.append(esc_id)
+
+    disponiveis = desempregados_com_opcao(professores)
+
     while (disponiveis != []):
         prof_id = disponiveis[0]
-        print("Antes")
-        professores[prof_id].info()
-        esc_id = melhor_escola_possivel(professores[prof_id])
-        escolas[esc_id].info()
-        escolas[esc_id].contratados.append(prof_id)
-        escolas[esc_id].habilitacoes_obtidas.append(professores[prof_id].habilitacoes)
+        esc_id = melhor_escola_possivel(prof_id)
 
-        professores[prof_id].contratado = 1
-        professores[prof_id].contratante = esc_id
+        print(vars(professores[prof_id]))
+        print(vars(escolas[esc_id]))
+        
+        contrata(esc_id, prof_id)
 
-        if (len(escolas[esc_id].contratados) > escolas[esc_id].vagas):
-            pior_aplicante_idx = escolas[esc_id].habilitacoes_obtidas.index(min(escolas[esc_id].habilitacoes_obtidas))
-            dispensa(pior_aplicante_idx, esc_id)
+        n_contratados = len(escolas[esc_id].contratados)
+        vagas = len(escolas[esc_id].habilitacoes_pretendidas)
 
-        if (len(escolas[esc_id].contratados) == escolas[esc_id].vagas):
-            hab = min(escolas[esc_id].habilitacoes_obtidas)
-            for prof in professores.values():
-                if (esc_id in prof.preferencias) and (prof.habilitacoes < hab):
-                    prof.preferencias.remove(esc_id)
+        if (n_contratados > vagas):
+            pior_idx = escolas[esc_id].habilitacoes_obtidas.index(min(escolas[esc_id].habilitacoes_obtidas))
+            dispensa(esc_id, pior_idx)
 
-        print("Depois")
-        professores[prof_id].info()
-        escolas[esc_id].info()
-        disponiveis = condition(professores)
+        if (n_contratados == vagas):
+            min_hab = min(escolas[esc_id].habilitacoes_obtidas)
+            for candidato in escolas[esc_id].candidatos_possiveis:
+                if professores[candidato].habilitacoes < min_hab:
+                    escolas[esc_id].candidatos_possiveis.remove(candidato)
+                    professores[candidato].rejeicoes.append(esc_id)
+
+
+        disponiveis = desempregados_com_opcao(professores)
 
 def teste():
     for esc in escolas.values():
